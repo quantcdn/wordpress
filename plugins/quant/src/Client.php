765914f843d286dcce9b1b9408347f4bc4839a5f
Kristen Pol <kristen.pol@gmail.com>
2025-11-06 19:09:50 -0800

    Handle empty data in constructor.

--- plugins/quant/src/Client.php
+++ plugins/quant/src/Client.php
@@ -20,17 +20,17 @@ class Client
     function deactivate() {}
 
     public function __construct() {
-        $settings = get_option(QUANT_SETTINGS_KEY);
-        $this->seedOptions = get_option(QUANT_SEED_KEY);
+        $settings = get_option(QUANT_SETTINGS_KEY) ?? [];
+        $this->seedOptions = get_option(QUANT_SEED_KEY) ?? [];
         $this->settings = $settings;
-        $this->webserver = $settings['webserver_url'];
-        $this->host = $settings['webserver_host'];
-        $this->endpoint = $settings['api_endpoint'] . '/v1';
+        $this->webserver = $settings['webserver_url'] ?? '';
+        $this->host = $settings['webserver_host'] ?? '';
+        $this->endpoint = $settings['api_endpoint'] . '/v1' ?? '';
         $this->headers['Content-type'] = 'application/json';
-        $this->headers['quant-project'] = $settings['api_project'];
-        $this->headers['quant-customer'] = $settings['api_account'];
-        $this->headers['quant-token'] = $settings['api_token'];
-        $this->disableTlsVerify = $settings['disable_tls_verify'];
+        $this->headers['quant-project'] = $settings['api_project'] ?? '';
+        $this->headers['quant-customer'] = $settings['api_account'] ?? '';
+        $this->headers['quant-token'] = $settings['api_token'] ?? '';
+        $this->disableTlsVerify = $settings['disable_tls_verify'] ?? '';
         $this->httpRequestTimeout = intval($settings['http_request_timeout']) ?? 15;
     }
 
